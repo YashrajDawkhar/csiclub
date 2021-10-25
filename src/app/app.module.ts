df68c792e2c409cb8da50b05b08ec423884f3920
Yashraj Dawkhar <yashrajdawkhar@gmail.com>
2021-10-26 03:59:27 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { EventsComponent } from './events/events.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    EventsComponent,
+    LandingPageComponent,
+    AboutUsComponent,
   ],
   imports: [
     CommonModule,
