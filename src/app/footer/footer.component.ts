8d9722e845aaf7e0a1353fc00f89cf4c3360a48a
Yashraj Dawkhar <yashrajdawkhar@gmail.com>
2021-10-26 16:36:31 +0530

    update

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  currentDate:any;
+
+  constructor() {
+    this.currentDate = new Date().getFullYear()
+   }
 
   ngOnInit(): void {
   }
